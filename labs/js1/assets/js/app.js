b6adc64685f8b716e71e429b83d79951bf731b53
Alicia Lee <aliciaklee@Alicias-MBP-2.wireless.newschool.edu>
2017-10-10 11:24:57 -0400

    js1

---
+++ labs/js1/assets/js/app.js
@@ -0,0 +1,36 @@
+var cash = 10000;
+
+update_wallet();
+
+$('.cash').html(cash)
+
+$('.item').click(function() {
+	var item   = $(this).data('item'),
+		amount = $(this).data('amount');
+
+
+	charge_card (amount, item);
+});
+
+//$('.item')
+//	click();
+//	function(){}
+//	click (function(){});
+
+
+function charge_card(amount,item) {
+	if (cash >= amount) {
+	// keep going
+		cash = cash - amount;
+		update_wallet();
+		$('.closet-items').append(item+',');
+	} else {
+		alert('Not enough money')
+	}
+
+}
+
+function update_wallet(){
+	$('.cash').html(cash);
+}
+
