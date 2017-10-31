590a130b4b2fcf64a6dbfa4454882b39b85659a6
Alicia Lee <aliciaklee@Alicias-MBP-2.wireless.newschool.edu>
2017-10-31 11:15:29 -0400

    edit track8

--- labs/js3/assets/js/app.js
+++ labs/js3/assets/js/app.js
@@ -15,7 +15,44 @@
 //});
 
 
-$('.nav-item').click (function(){
-	var button_name = $(this).data('horse');
-	alert(button_name);
+//$('.nav-item').click(function() {
+//	var target_horse = $(this).data('horse');
+//	$(target_horse).removeClass('hide');
+//});
+
+
+//$('.nav-item').click(function(){
+//	var target = $(this).data('horse');
+//	var img_to_show;
+//
+//	if (target == 'apple') {
+//		img_to_show = '<img class="apple" src="assets/img/apple.png" />';
+//	} else if (target == 'nike') {
+//		img_to_show = '<img class="nike" src="assets/img/nike.png" />';
+//	} else if (target == 'disney') {
+//		img_to_show = '<img class="disney" src="assets/img/disney.png" />';
+//	} else {
+//		img_to_show = '<img class="vogue" src="assets/img/vogue.png" />';
+//	}
+//
+//	$('.content').append(img_to_show);
+//
+//});
+
+
+
+$('.nav-item').click(function(){
+	var img         = $(this).data('img');
+	var class_name  = $(this).data('classname');
+	var img_to_show = '<div class ="'+ class_name +' logo"> <img src="assets/img/' + img +'" /> </div>';
+
+	$('.content').prepend(img_to_show);
+
 });
+
+
+
+
+
+
+
